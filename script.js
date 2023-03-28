07f11133b216bf033bf10718334a3d607214d778
Janice Thomas <janicethomas2004@gmail.com>
2023-03-28 07:49:32 -0500

    vars declared

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
     //set variables for time and activities
+    let actStart = [
+        [,],
+        [,],
+        [,],
+        [,],
+        [,]
+    ];
+    let actDur = [
+        [,],
+        [,],
+        [,],
+        [,],
+        [,]
+    ];
+    let todoDur = [
+        [,],
+        [,],
+        [,],
+        [,],
+        [,]
+    ];
 
     
 })
